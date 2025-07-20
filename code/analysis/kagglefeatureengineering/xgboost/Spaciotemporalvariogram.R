ccc02d210d9c9242aa73bde79fe5b7f06e4057b6
Sebastian Dohne <sed24@ic.ac.uk>
2025-07-20 22:50:16 +0100

    test and val push

--- code/analysis/kagglefeatureengineering/xgboost/Spaciotemporalvariogram.R
+++ code/analysis/kagglefeatureengineering/xgboost/Spaciotemporalvariogram.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(sp)
 library(spacetime)
 library(gstat)
+library(ggplot2)
 
 # Load and clean
 df_clean <- read.csv("../../../../data/finaldatasets/testdata/xgboost.csv") %>%
@@ -28,17 +29,32 @@ stidf <- STIDF(coords, time_vals, data = data.frame(yield = df_agg$yield))
 # ⬇️ Variogram computation (conservative settings)
 vg_st <- variogramST(yield ~ 1, data = stidf,
                      width = 1,  # ~1 degree bins for latitude/longitude
-                     tlags = c(0, 365, 730),  # lag of 0, 1, 2 years
+                     tlags = c(0, 365, 730, (365 * 3), (365 * 5), (365 * 8), (365 * 11), (365 * 15)),  # lag of 0, 1, 2 years
                      assumeRegular = FALSE)
 
+#Metrices obtained by the the variogram
 summary(vg_st)
 head(vg_st)
 anyNA(vg_st)
 colSums(is.na(vg_st))
+table(vg_st$tlag)
 
+
+#remove NAs
 vg_st_clean <- subset(vg_st, !is.na(gamma) & !is.na(dist) & !is.na(np))
+# Remove bins with not enough point pairs for statistical reliability
+vg_st_clean <- subset(vg_st, !is.na(gamma) & !is.na(dist) & !is.na(np) & np >= 5)
+
 
 # ⬇️ Plot
-plot(vg_st_clean, wireframe = TRUE, main = "Spatiotemporal Variogram")
 
+
+ggplot(vg_st_clean, aes(x = dist, y = timelag, fill = gamma)) +
+  geom_tile(height = 250, width = 250) +  # smooth blocks
+  scale_fill_viridis_c(option = "D", na.value = "transparent") +
+  labs(title = "Spatiotemporal Semivariance",
+       x = "Distance (km)",
+       y = "Time Lag (days)",
+       fill = "Gamma") +
+  theme_minimal()
 
